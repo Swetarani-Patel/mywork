a72f9135340e07e78cdd2b6abda8b348e0883c7b
Swetarani-Patel <swetaranipatel7@gmail.com>
2023-07-08 19:24:24 +0530

    travel

---
+++ travelapp/src/App.js
@@ -0,0 +1,19 @@
+// import logo from './logo.svg';
+import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import Main from './Components/Main/Main';
+import Footer from './Components/Footer/Footer';
+import Home from './Components/Home/Home';
+function App() {
+  return (
+    <div className="App">
+    <Navbar/>
+    {/* <Main/>
+    <Footer/> */}
+    <Home/>
+    </div>
+  );
+}
+
+export default App;
+
